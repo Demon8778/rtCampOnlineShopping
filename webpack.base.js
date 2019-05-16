2ea3c6ee834e3faacd3245c84e2d8b5eb5429088
Kirankumar Ahir <kirankatariya8778@gmail.com>
2019-05-16 21:54:27 +0530

    Setting webpack configuration for client and server

---
+++ webpack.base.js
@@ -0,0 +1,22 @@
+module.exports = {
+	module: {
+		rules: [
+			{
+				test: /\.jsx?$/,
+				exclude: /node_modules/,
+				use: {
+					loader: 'babel-loader',
+					options: {
+						presets: [
+							[
+								'@babel/preset-env',
+								{ targets: { browsers: ['last 2 versions'] } }
+							],
+							'@babel/preset-react'
+						]
+					}
+				}
+			}
+		]
+	}
+};
