d4e7b35b5b66c9efffea5cd6ff279a7ad08161de
Kirankumar Ahir <kirankatariya8778@gmail.com>
2019-05-23 15:34:13 +0530

    Chagin url

--- src/client/context.js
+++ src/client/context.js
@@ -148,7 +148,7 @@ class PrdouctProvider extends Component {
 
 	clearCart = async () => {
 		const { data } = await axios.get(
-			'https://rtcamp-online-shopping.herokuapp.com//products'
+			'https://rtcamp-online-shopping.herokuapp.com/products'
 		);
 		this.setState(() => {
 			return {
